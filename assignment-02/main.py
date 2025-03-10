71ebcc5e55a827394f772c11a6bdfe1c31ed3ac6
Personal <faisaltez@gmail.com>
2025-03-10 23:57:33 +0600

    assignment-02

---
+++ assignment-02/main.py
@@ -0,0 +1,34 @@
+# 1. Print your name.
+print("Faisal")
+
+# 2. Input your batch number using input() function.
+batch_number = input("Enter your batch number: ")
+
+# 3. Create 5 different valid Variables and print.
+name = "Faisal"
+age = 25
+height = 5.8
+is_developer = True
+skills = ["Php","Laravel","Python", "Django", "Web Development"]
+
+print(name, age, height, is_developer, skills)
+
+# 4. Create Pascal Case Variable.
+PascalCaseVariable = "This is Pascal Case"
+
+# 5. Assign a single value to Multiple variables.
+m = n = o = "Django"
+
+print(m, n, o)
+
+# 6. Input float value using input().
+float_value = float(input("Enter a float value: "))
+print(float_value)
+
+# 7. Convert string to upper case.
+a = "Welcome to Django for web and ai"
+print(a.upper())
+
+# 8. Remove whitespace from string.
+b = "   Python"
+print(b.strip())
